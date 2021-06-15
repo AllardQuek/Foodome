a0eb4ef856703ba2aa05c63286dc41a859c2a047
Allard <AllardQuek@users.noreply.github.com>
2021-06-15 20:48:54 +0800

    moved group orders screen

---
+++ screens/GroupOrders.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GroupOrders = () => {
+  return (
+    <div>
+      <Text>Order No. 1: Chocolate Cake</Text>
+    </div>
+  );
+};
+
+export default GroupOrders;
