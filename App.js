5e4dc2bca6ed7fd22b07c0790e4bc3e1045f0ac3
Allard <AllardQuek@users.noreply.github.com>
2021-06-15 20:32:12 +0800

    renamed CustomerOrVendor to StartUp screen

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 
-import {Restaurant, OrderDelivery, CustomerOrVendor} from "./screens";
+import { Restaurant, OrderDelivery, StartUp } from "./screens";
 import Tabs from "./navigation/tabs";
 
 const Stack = createStackNavigator();
@@ -26,9 +26,9 @@ const App = () => {
         screenOptions={{
           headerShown: false,
         }}
-        initialRouteName={"CustomerOrVendor"}
+        initialRouteName={"StartUp"}
       >
-        <Stack.Screen name="CustomerOrVendor" component={CustomerOrVendor} />
+        <Stack.Screen name="StartUp" component={StartUp} />
         <Stack.Screen name="Home" component={Tabs} />
         <Stack.Screen name="Restaurant" component={Restaurant} />
         <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
